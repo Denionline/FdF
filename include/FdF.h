d63cc0c159820143e2c1aa1ee72513f7c3eac0f4
Denionline <danielximenes164@gmail.com>
2025-06-22 21:37:43 +0100

    feat: New branch

--- include/FdF.h
+++ include/FdF.h
@@ -6,17 +6,26 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 22:37:46 by dximenes          #+#    #+#             */
-/*   Updated: 2025/06/20 18:49:41 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/06/22 20:26:00 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_FDF
 #define FT_FDF
 
-#include "minilibx-linux/mlx.h"
 #include "get_next_line/include/get_next_line.h"
 #include "libft/include/libft.h"
+#include "minilibx-linux/mlx.h"
 #include <stdlib.h>
+
+typedef struct s_draw
+{
+	int x0;
+	int x1;
+	int y0;
+	int y1;
+	int pad;
+} t_draw;
 
 typedef struct s_map
 {
@@ -36,8 +45,9 @@ typedef struct s_window
 typedef struct s_mlx
 {
 	void *	   connection;
-	t_window * window;
 	t_map *	   map;
+	t_window * window;
+	t_draw *   draw;
 } t_mlx;
 
 void render(t_mlx * mlx);
--- render.c
+++ render.c
@@ -6,48 +6,75 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 11:01:13 by dximenes          #+#    #+#             */
-/*   Updated: 2025/06/21 18:42:27 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/06/22 20:26:07 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FdF.h"
 
-static void draw_lines(t_mlx * mlx)
+static void projeta(int x, int y, int z, int * sx, int * sy, int pad, int offx, int offy)
 {
+	*sx = offx + x * pad;
+	*sy = offy + y * pad - z;
 }
 
-static void draw_points(t_mlx * mlx, int x, int y, int pad)
+static void bresenham(void * conn, void * win, int x0, int y0, int x1, int y1, int color)
 {
-	if (mlx->map->coordinates[y][x] == 0)
-		mlx_pixel_put(
-			mlx->connection,
-			mlx->window->content,
-			x * pad,
-			y * pad,
-			0x00ff00);
-	else
-		mlx_pixel_put(
-			mlx->connection,
-			mlx->window->content,
-			x * pad,
-			y * pad,
-			0x0000ff);
+	int dx = abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+	int dy = -abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
+	int err = dx + dy, e2;
+
+	while (1)
+	{
+		mlx_pixel_put(conn, win, x0, y0, color);
+		if (x0 == x1 && y0 == y1)
+			break;
+		e2 = 2 * err;
+		if (e2 >= dy)
+		{
+			err += dy;
+			x0 += sx;
+		}
+		if (e2 <= dx)
+		{
+			err += dx;
+			y0 += sy;
+		}
+	}
 }
 
 void render(t_mlx * mlx)
 {
-	int pad;
-	int x;
-	int y;
+	int pad = 20;
+	int offx = mlx->window->vw / 3;
+	int offy = mlx->window->vh / 3;
+	int x, y;
+	int sx0, sy0, sx1, sy1;
+	int z0, z1;
+	int color;
 
-	pad = 30;
 	y = 0;
 	while (y < mlx->map->size_y)
 	{
 		x = 0;
 		while (x < mlx->map->size_x)
 		{
-			draw_points(mlx, x, y, pad);
+			z0 = mlx->map->coordinates[y][x];
+			color = (z0 == 0 ? 0x00FF00 : 0x0000FF);
+			projeta(x, y, z0, &sx0, &sy0, pad, offx, offy);
+			mlx_pixel_put(mlx->connection, mlx->window->content, sx0, sy0, color);
+			if (x + 1 < mlx->map->size_x)
+			{
+				z1 = mlx->map->coordinates[y][x + 1];
+				projeta(x + 1, y, z1, &sx1, &sy1, pad, offx, offy);
+				bresenham(mlx->connection, mlx->window->content, sx0, sy0, sx1, sy1, color);
+			}
+			if (y + 1 < mlx->map->size_y)
+			{
+				z1 = mlx->map->coordinates[y + 1][x];
+				projeta(x, y + 1, z1, &sx1, &sy1, pad, offx, offy);
+				bresenham(mlx->connection, mlx->window->content, sx0, sy0, sx1, sy1, color);
+			}
 			x++;
 		}
 		y++;
