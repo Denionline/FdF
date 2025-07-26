1bd334fc4787608474f5a2ab0c186adeae5453c3
denionline <danielximenes164@gmail.com>
2025-07-26 15:53:32 +0100

    feat: Separated the draw controls on the static functions to have no more than 25 lines

--- src/renderer/draw/draw_art_menu.c
+++ src/renderer/draw/draw_art_menu.c
@@ -6,7 +6,7 @@
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 18:01:46 by dximenes          #+#    #+#             */
-/*   Updated: 2025/07/26 12:53:17 by dximenes         ###   ########.fr       */
+/*   Updated: 2025/07/26 15:52:50 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,36 @@ static int draw_art(t_head * h, int column, int row, t_vars_art art)
 	return (row + i);
 }
 
+static void draw_zoom_arts(t_head * h, int default_column, int * row)
+{
+	*row = draw_art(h, default_column + 4, *row, h->menu.arts.zoom_title);
+	*row += 1;
+	draw_art(h, default_column + 2, *row, h->menu.arts.key_plus);
+	draw_art(h, default_column + 6, *row, h->menu.arts.key_minus);
+}
+
+static void draw_rotation_arts(t_head * h, int default_column, int * row)
+{
+	*row = draw_art(h, default_column + 3, *row, h->menu.arts.rotation_title);
+	*row += 1;
+	draw_art(h, default_column, *row, h->menu.arts.key_q);
+	draw_art(h, default_column + 4, *row, h->menu.arts.key_w);
+	*row = draw_art(h, default_column + 8, *row, h->menu.arts.key_e);
+	draw_art(h, default_column, *row, h->menu.arts.key_a);
+	draw_art(h, default_column + 4, *row, h->menu.arts.key_s);
+	draw_art(h, default_column + 8, *row, h->menu.arts.key_d);
+}
+
+static void draw_move_arts(t_head * h, int default_column, int * row)
+{
+	*row = draw_art(h, default_column + 4, *row, h->menu.arts.move_title);
+	*row += 1;
+	*row = draw_art(h, default_column + 4, *row, h->menu.arts.key_up);
+	draw_art(h, default_column, *row, h->menu.arts.key_left);
+	draw_art(h, default_column + 4, *row, h->menu.arts.key_down);
+	draw_art(h, default_column + 8, *row, h->menu.arts.key_right);
+}
+
 void draw_art_menu(t_head * h)
 {
 	const int default_column = 4;
@@ -38,31 +68,10 @@ void draw_art_menu(t_head * h)
 	row = 0;
 	row = draw_art(h, default_column - 3, row + 1, h->menu.arts.key_esc);
 	row = draw_art(h, default_column, row + 2, h->menu.arts.logo);
-
 	row += 50;
-
-	row = draw_art(h, default_column + 4, row, h->menu.arts.zoom_title);
-	row += 1;
-	draw_art(h, default_column + 2, row, h->menu.arts.key_plus);
-	draw_art(h, default_column + 6, row, h->menu.arts.key_minus);
-
+	draw_zoom_arts(h, default_column, &row);
 	row += 6;
-
-	row = draw_art(h, default_column + 3, row, h->menu.arts.rotation_title);
-	row += 1;
-	draw_art(h, default_column, row, h->menu.arts.key_q);
-	draw_art(h, default_column + 4, row, h->menu.arts.key_w);
-	row = draw_art(h, default_column + 8, row, h->menu.arts.key_e);
-	draw_art(h, default_column, row, h->menu.arts.key_a);
-	draw_art(h, default_column + 4, row, h->menu.arts.key_s);
-	draw_art(h, default_column + 8, row, h->menu.arts.key_d);
-
+	draw_rotation_arts(h, default_column, &row);
 	row += 6;
-
-	row = draw_art(h, default_column + 4, row, h->menu.arts.move_title);
-	row += 1;
-	row = draw_art(h, default_column + 4, row, h->menu.arts.key_up);
-	draw_art(h, default_column, row, h->menu.arts.key_left);
-	draw_art(h, default_column + 4, row, h->menu.arts.key_down);
-	draw_art(h, default_column + 8, row, h->menu.arts.key_right);
+	draw_move_arts(h, default_column, &row);
 }
