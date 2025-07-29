7eb3bf5553b8069d33d3f6885b5c3a8bf314e920
Denionline <danielximenes164@gmail.com>
2025-07-29 10:13:41 +0100

    fix: Fixed the conflit comment on file

--- src/parse/end.c
+++ src/parse/end.c
@@ -5,12 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dximenes <dximenes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/11 17:31:48 by dximenes          #+#    #+#             */
-<<<<<<<< HEAD:src/parse/verify.c
-/*   Updated: 2025/07/26 17:22:59 by dximenes         ###   ########.fr       */
-========
-/*   Updated: 2025/07/29 09:58:09 by dximenes         ###   ########.fr       */
->>>>>>>> matrix:src/parse/end.c
+/*   Created: 2025/07/29 10:13:07 by dximenes          #+#    #+#             */
+/*   Updated: 2025/07/29 10:13:21 by dximenes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +16,6 @@ void	end(t_head *head)
 {
 	int	i;
 
-<<<<<<<< HEAD:src/parse/verify.c
-	mlx_destroy_image(head->vars.mlx, head->draw->image.img);
-	mlx_destroy_window(head->vars.mlx, head->vars.win);
-	mlx_destroy_display(head->vars.mlx);
-	i = 0;
-	while (head->map->points[0] && i < head->map->size.y)
-		free(head->map->points[i++]);
-	if (head->map->points)
-		free(head->map->points);
-	if (head->vars.mlx)
-		free(head->vars.mlx);
-========
 	if (head->menu.image.img)
 		mlx_destroy_image(head->vars.mlx, head->menu.image.img);
 	if (head->draw->image.img)
@@ -45,7 +29,6 @@ void	end(t_head *head)
 		free(head->map->points[i++]);
 	if (head->map->points)
 		free(head->map->points);
->>>>>>>> matrix:src/parse/end.c
 	if (head->map)
 		free(head->map);
 	if (head->draw)
